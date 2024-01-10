78b90c18a6a131e60eddca65d71b932a16d22b18
ahmedeldamity <ahmedeldamity25@gmail.com>
2024-01-11 00:36:27 +0200

    Project Structure

---
+++ API/ServicesExtension/SwaggerServiceExtension.cs
@@ -0,0 +1,23 @@
+﻿namespace API.ServicesExtension
+{
+    public class SwaggerServiceExtension
+    {
+        public static IServiceCollection AddSwaggerServices (IServiceCollection services)
+        {
+            services.AddEndpointsApiExplorer();
+
+            services.AddSwaggerGen();
+
+            return services;
+        }
+        
+        public static WebApplication AddSwaggerServices (WebApplication app)
+        {
+            app.UseSwagger();
+
+            app.UseSwaggerUI();
+
+            return app;
+        }
+    }
+}
