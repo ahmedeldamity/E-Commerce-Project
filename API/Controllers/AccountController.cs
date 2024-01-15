bcd8ca277c9d6e181c00e53e46ef7a02ab32ed23
ahmedeldamity <ahmedeldamity25@gmail.com>
2024-01-15 12:59:37 +0200

    Login Endpoint

---
+++ API/Controllers/AccountController.cs
@@ -0,0 +1,42 @@
+﻿using API.Dtos;
+using API.Errors;
+using Core.Entities.Identity;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+
+namespace API.Controllers
+{
+    public class AccountController : BaseApiController
+    {
+        private readonly UserManager<AppUser> _userManager;
+        private readonly SignInManager<AppUser> _signInManager;
+
+        public AccountController(UserManager<AppUser> userManager, SignInManager<AppUser> signInManager)
+        {
+            _userManager = userManager;
+            _signInManager = signInManager;
+        }
+
+        [HttpPost("login")]
+        public async Task<ActionResult<AppUserDto>> Login(LoginDto model)
+        {
+            var user = await _userManager.FindByEmailAsync(model.Email);
+
+            if (user is null)
+                return Unauthorized(new ApiResponse(401));
+
+            var result = await _signInManager.CheckPasswordSignInAsync(user, model.Password, false);
+
+            if (result.Succeeded is false)
+                return Unauthorized(new ApiResponse(401));
+
+            return Ok(new AppUserDto
+            {
+                DisplayName = user.DisplayName,
+                Email = model.Email,
+                Token = "Here Is Will Be Token"
+            });
+        }
+    }
+}
