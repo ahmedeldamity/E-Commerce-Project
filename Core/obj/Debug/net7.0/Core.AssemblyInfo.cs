9b107dd6bc3eca64b146f9eddf5c0cc44b1473fa
Ahmed Eldamity <ahmedeldamity25@gmail.com>
2024-07-18 14:36:18 +0300

    Last modifications

--- Core/obj/Debug/net7.0/Core.AssemblyInfo.cs
+++ Core/obj/Debug/net7.0/Core.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Core")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6b086183965d4506a0c9c5a428b6fe91112e8189")]
 [assembly: System.Reflection.AssemblyProductAttribute("Core")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Core")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
